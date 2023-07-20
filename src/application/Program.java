b3f5589c1d4adcf356a51f15b4fb3e2e844ed5a3
Bruno dos Reis Sola <brunosola3@gmail.com>
2023-07-19 21:55:49 -0300

    Implementação da partida, peça no tabuleiro, cor e layout tabuleiro no console.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,15 @@
 package application;
 
+import boardgame.Board;
+import boardgame.Position;
+import chess.model.entities.ChessMatch;
+
 public class Program {
     public static void main(String[] args) {
-        
+
+        ChessMatch chessMatch = new ChessMatch();
+
+        UI.printBoard(chessMatch.getPieces());
+
     }
 }
---
+++ src/chess/model/enums/Color.java
@@ -0,0 +1,6 @@
+package chess.model.enums;
+
+public enum Color {
+    BLACK,
+    WHITE;
+}
