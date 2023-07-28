d368a221693522932ad581abfb56ae45093040ea
Bruno dos Reis Sola <brunosola3@gmail.com>
2023-07-28 00:07:28 -0300

    Contagem de movimentos das peças.

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -55,7 +55,8 @@ public class ChessMatch {
     }
 
     private Piece makeMove(Position source, Position target){
-        Piece p = board.removePiece(source);
+        ChessPiece p = (ChessPiece)board.removePiece(source);
+        p.increaseMoveCount();
         Piece capturedPiece = board.removePiece(target);
         board.placePiece(p, target);
         if (capturedPiece != null) {
@@ -63,6 +64,18 @@ public class ChessMatch {
             capturedPieces.add(capturedPiece);
         }
         return capturedPiece;
+    }
+
+    private void undoMove(Position source, Position target, Piece capturedPiece){
+        ChessPiece p = (ChessPiece) board.removePiece(target);
+        p.decreaseMoveCount();
+        board.placePiece(p, source);
+
+        if (capturedPiece == null){
+            board.placePiece(capturedPiece, target);
+            capturedPieces.remove(capturedPiece);
+            piecesOnTheBoard.add(capturedPiece);
+        }
     }
 
     private void validateSourcePosition(Position position){
--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -9,6 +9,7 @@ import org.ietf.jgss.Oid;
 public abstract class ChessPiece extends Piece {
 
     private Color color;
+    private int moveCount;
 
     public ChessPiece(Board board, Color color) {
         super(board);
@@ -16,6 +17,13 @@ public abstract class ChessPiece extends Piece {
     }
     public Color getColor() {
         return color;
+    }
+
+    public void increaseMoveCount(){
+        moveCount++;
+    }
+    public void decreaseMoveCount(){
+        moveCount--;
     }
 
     protected boolean isThereOpponentPiece(Position position){
